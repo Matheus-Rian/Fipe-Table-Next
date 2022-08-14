0fea26fd190fa07549ea1342dfc12f16af3b96ba
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-13 22:53:37 -0300

    feat: create new Table Fipe

---
+++ src/components/TableFipe.tsx
@@ -0,0 +1,50 @@
+import { Box, TextField, MenuItem, Button } from "@mui/material"
+import React, { useState } from "react"
+
+export const TableFipe: React.FC = () => {
+  const [country, setCountry] = useState('');
+  console.log({ country });
+
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setCountry(ev.target.value as string);
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: "center",
+        alignItems: "center",
+        width: 400,
+        backgroundColor: 'white',
+        marginTop: '16px',
+      }}
+    >
+      <TextField
+        label='Select Country'
+        select
+        value={country}
+        onChange={handleChange}
+        sx={{ minWidth: 300, marginBottom: '16px', marginTop: '24px' }}
+      >
+        <MenuItem value='IN'>India</MenuItem>
+        <MenuItem value='BR'>Brasil</MenuItem>
+      </TextField>
+
+      <TextField
+        label='Select Country'
+        select
+        value={country}
+        onChange={handleChange}
+        sx={{ minWidth: 300, marginBottom: '16px' }}
+      >
+        <MenuItem value='IN'>India</MenuItem>
+        <MenuItem value='BR'>Brasil</MenuItem>
+      </TextField>
+
+      <Button sx={{ marginBottom: '24px' }} variant='contained' disabled>Consultar Preço</Button>
+
+    </Box>
+  )
+}
