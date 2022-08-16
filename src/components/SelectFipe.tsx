3ecb8a3e70ee114b312ea8b1de233ff55086c5ae
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-16 17:50:59 -0300

    feat: create component SelectFipe

---
+++ src/components/SelectFipe.tsx
@@ -0,0 +1,39 @@
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material"
+import { ICarsBrands } from "../hooks/useCarsBrands";
+
+interface ISelectProps<T> {
+  id: string;
+  label: string;
+  value: string;
+  selectItens: T;
+  onChange: (ev: SelectChangeEvent<string>, child: React.ReactNode) => void;
+  isDisabled?: boolean;
+}
+
+export const SelectFipe = <T extends ICarsBrands[] | undefined>(
+  { id, label, value, selectItens, onChange, isDisabled = false }: ISelectProps<T>
+) => {
+
+  return (
+    <FormControl sx={{ minWidth: 300, marginBottom: '16px', marginTop: '24px' }}>
+      <InputLabel id={id}>{label}</InputLabel>
+      <Select
+        labelId={id}
+        id={id}
+        value={value}
+        label={label}
+        onChange={onChange}
+        disabled={isDisabled}
+      >
+        {selectItens?.map(({ codigo, nome }) => (
+          <MenuItem
+            key={codigo}
+            value={nome}
+          >
+            {nome}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  )
+}
