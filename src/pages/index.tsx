1ba5dc586a9583f6a2979a92b5f3ddcdcc05c24a
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-16 22:09:39 -0300

    feat: add CarContextProvider in Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import GlobalStyle, { Container } from '../styles/global';
 import { TableFipe } from '../components/TableFipe';
+import { CarContextProvider } from '../contexts/CarContext';
 
 const Home: NextPage = () => {
   return (
     <>
-      <GlobalStyle />
-      <Head>
-        <title>Home | Fipe</title>
-        <meta name="description" content="Project Fipe-Table with Next" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Container>
-        <h1 style={{ marginBottom: '12px' }}>Tabela Fipe</h1>
-        <h3>Consulte o valor de um veículo de forma gratuita</h3>
-        <TableFipe />
-      </Container>
+      <CarContextProvider>
+        <GlobalStyle />
+        <Head>
+          <title>Home | Fipe</title>
+          <meta name="description" content="Project Fipe-Table with Next" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Container>
+          <h1 style={{ marginBottom: '12px' }}>Tabela Fipe</h1>
+          <h3>Consulte o valor de um veículo de forma gratuita</h3>
+          <TableFipe />
+        </Container>
+      </CarContextProvider>
     </>
   )
 }
