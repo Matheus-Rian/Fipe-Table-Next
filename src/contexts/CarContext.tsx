c1cac1011ea10d59c4d39b2775c1d5575d2cdcc9
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-18 11:43:14 -0300

    feat: add in context tableFipeResult

--- src/contexts/CarContext.tsx
+++ src/contexts/CarContext.tsx
@@ -6,9 +6,23 @@ type CarCodes = {
   year: string,
 }
 
+export type TableFipeResult = {
+  AnoModelo: number,
+  CodigoFipe: string,
+  Combustivel: string,
+  Marca: string,
+  MesReferencia: string,
+  Modelo: string,
+  SiglaCombustivel: string,
+  TipoVeiculo: number
+  Valor: string,
+}
+
 interface ICarContext {
   carCodes: CarCodes,
   updateCarCodes: (updateData: Partial<CarCodes>) => void,
+  tableFipeResult: TableFipeResult | null,
+  storeTableFipeResult: (data: TableFipeResult) => void,
 }
 
 export const CarContext = createContext<ICarContext | null>(null);
@@ -20,6 +34,12 @@ export const CarContextProvider = ({ children }: { children: React.ReactNode }) 
     year: '',
   });
 
+  const [tableFipeResult, setTableFipeResult] = useState<TableFipeResult | null>(null);
+
+  function storeTableFipeResult(data: TableFipeResult) {
+    setTableFipeResult(data);
+  }
+
   function updateCarCodes(data: Partial<CarCodes>) {
     setCarCodes((prev) => ({...prev, ...data}));
   }
@@ -29,6 +49,8 @@ export const CarContextProvider = ({ children }: { children: React.ReactNode }) 
       value={{
         updateCarCodes,
         carCodes,
+        tableFipeResult,
+        storeTableFipeResult,
       }}
     >
       {children}
