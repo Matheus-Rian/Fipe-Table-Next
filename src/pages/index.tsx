0773dac47e0ed55cf4da72e7cccacde62bcdc4eb
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-18 10:55:05 -0300

    feat: removed context and GlobalStyle to the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import GlobalStyle, { Container } from '../styles/global';
+import { Container } from '../styles/global';
 import { TableFipe } from '../components/TableFipe';
-import { CarContextProvider } from '../contexts/CarContext';
 
 const Home: NextPage = () => {
   return (
     <>
-      <CarContextProvider>
-        <GlobalStyle />
-        <Head>
-          <title>Home | Fipe</title>
-          <meta name="description" content="Project Fipe-Table with Next" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Container>
-          <h1 style={{ marginBottom: '12px' }}>Tabela Fipe</h1>
-          <h3>Consulte o valor de um veículo de forma gratuita</h3>
-          <TableFipe />
-        </Container>
-      </CarContextProvider>
+      <Head>
+        <title>Home | Fipe</title>
+        <meta name="description" content="Project Fipe-Table with Next" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <h1 style={{ marginBottom: '12px' }}>Tabela Fipe</h1>
+        <h3>Consulte o valor de um veículo de forma gratuita</h3>
+        <TableFipe />
+      </Container>
     </>
   )
 }
